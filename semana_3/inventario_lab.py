d26a06b015a071282a1a01404d7546cabb951326
GamaA185 <gamaacl@eq.ufrj.br>
2026-05-31 11:26:32 -0300

    Adicionando base de dados

---
+++ semana_3/inventario_lab.py
@@ -0,0 +1,7 @@
+import sys
+
+reagentes_unicos = set(reagentes)
+
+print("=== REAGENTES DISPONIVEIS ===")
+print(reagentes_unicos)
+print(f"Quantidade de reagentes diferentes: {len(reagentes_unicos)}")

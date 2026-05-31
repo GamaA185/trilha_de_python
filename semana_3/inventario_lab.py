eba5ff51c35a28bd4620238f32c9aa5bf502e8fc
GamaA185 <gamaacl@eq.ufrj.br>
2026-05-31 11:57:23 -0300

    zip e unpacking

--- semana_3/inventario_lab.py
+++ semana_3/inventario_lab.py
@@ -5,3 +5,15 @@ reagentes_unicos = set(reagentes)
 print("=== REAGENTES DISPONIVEIS ===")
 print(reagentes_unicos)
 print(f"Quantidade de reagentes diferentes: {len(reagentes_unicos)}")
+
+inventario = list(zip(reagentes, lotes, purezas))
+
+
+print("\n=== RELATORIO DO INVENTARIO ===")
+
+for reagente, lote, pureza in inventario:
+    print(
+        f"Frasco do Lote: {lote} | "
+        f"Reagente: {reagente} | "
+        f"Pureza: {pureza}%"
+    )
